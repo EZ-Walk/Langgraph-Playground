9e86eeba8bcbf7d43d49ec22f6a297298fb0ff19
EZ-Walk <rocketwalker@gmail.com>
2025-04-11 20:47:29 -0700

    human-in-the-loop with in-memory checkpointing only works in the notebook.

--- main.py
+++ main.py
@@ -6,14 +6,14 @@ from typing import Annotated
 
 from langchain_anthropic import ChatAnthropic
 from langchain_community.tools.tavily_search import TavilySearchResults
-from langchain_core.messages import BaseMessage
+from langchain_core.tools import tool
 from typing_extensions import TypedDict
 
-from langgraph.graph import StateGraph
+from langgraph.checkpoint.memory import MemorySaver
+from langgraph.graph import StateGraph, START, END
 from langgraph.graph.message import add_messages
 from langgraph.prebuilt import ToolNode, tools_condition
-
-from langgraph.checkpoint.memory import MemorySaver
+from langgraph.types import Command, interrupt
 
 
 class State(TypedDict):
@@ -23,35 +23,45 @@ class State(TypedDict):
 graph_builder = StateGraph(State)
 
 
+@tool
+def human_assistance(query: str) -> str:
+    """Request assistance from a human."""
+    human_response = interrupt({"query": query})
+    return human_response["data"]
+
+
 tool = TavilySearchResults(max_results=2)
-tools = [tool]
+tools = [tool, human_assistance]
 llm = ChatAnthropic(model="claude-3-5-sonnet-20240620")
 llm_with_tools = llm.bind_tools(tools)
 
 
 def chatbot(state: State):
-    return {"messages": [llm_with_tools.invoke(state["messages"])]}
+    message = llm_with_tools.invoke(state["messages"])
+    assert(len(message.tool_calls) <= 1)
+    return {"messages": [message]}
 
 
 graph_builder.add_node("chatbot", chatbot)
 
-tool_node = ToolNode(tools=[tool])
+tool_node = ToolNode(tools=tools)
 graph_builder.add_node("tools", tool_node)
 
 graph_builder.add_conditional_edges(
     "chatbot",
     tools_condition,
 )
-# Any time a tool is called, we return to the chatbot to decide the next step
 graph_builder.add_edge("tools", "chatbot")
-graph_builder.set_entry_point("chatbot")
+graph_builder.add_edge(START, "chatbot")
+
 memory = MemorySaver()
 graph = graph_builder.compile(checkpointer=memory)
 
 def stream_graph_updates(user_input: str):
     for event in graph.stream(
         {"messages": [{"role": "user", "content": user_input}]},
-        config={"configurable": {"thread_id": "1"}}
+        config={"configurable": {"thread_id": "1"}},
+        
     ):
         for value in event.values():
             print("Assistant:", value["messages"][-1].content)
@@ -65,9 +75,6 @@ if __name__ == "__main__":
                 print("Goodbye!")
                 break
             stream_graph_updates(user_input)
-        except:
-            # fallback if input() is not available
-            user_input = "What do you know about LangGraph?"
-            print("User: " + user_input)
-            stream_graph_updates(user_input)
+        except Exception as e:
+            raise e
             break
